88c2aff323cf6756c322cbf0ab89b67c94edccb5
Stanislaw Babkin <stalissss@outlook.com>
2018-12-11 00:00:30 +0300

    Remove default project files

--- src/Zlang.Bytecode/Library.fs
+++
@@ -1,5 +0,0 @@
-namespace Zlang.Bytecode
-
-module Say =
-    let hello name =
-        printfn "Hello %s" name
--- src/Zlang.VM/Library.fs
+++
@@ -1,5 +0,0 @@
-namespace Zlang.VM
-
-module Say =
-    let hello name =
-        printfn "Hello %s" name
