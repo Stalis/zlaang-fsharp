b64af5f872f5ac9ae1e9a8630c2a2fd8f359a008
Stanislaw Babkin <stalissss@outlook.com>
2018-12-11 11:45:16 +0300

    Refactor test program

--- src/Zlang.Cli/Program.fs
+++ src/Zlang.Cli/Program.fs
@@ -6,43 +6,31 @@ open Zlang.VM
 open System.Xml
 
 let bin_prog = [
-    0x00; // 0 NOP
-    0x10; // 1 Load.c
-    0x04; // 2 4
-    0x02; // 3 Dup
-    0x42; // 4 Jump.nz
-    0x08; // 5 8
+(*0x0*)    Nop;                 // Nop
+(*0x1*)    Load_c; 0x04;        // Load.c 4
+(*0x3*)    Dup;                 // Dup
+(*0x4*)    Jump_nz; 0x08;       // Jump.nz 0x08
 
-    0x32; // 6 Mul
-    0xFF; // 7 Halt
-    0x10; // 8 Load.c
-    0x00; // 9 0
+(*0x6*)    Mul;                 // Mul
+(*0x7*)    Halt;                // Halt
+(*0x8*)    Load_c; 0x00;        // Load.c 0
 
-    0x40; // A Jump
-    0x0F; // B F
+(*0xA*)    Jump; 0x0F;          // Jump 15
 
-    0x00; // C NOP
-    0x10; // D Load.c
-    0x09; // E 9
+(*0xC*)    Nop;                 // Nop
+(*0xD*)    Load_c; 0x09;        // Load.c 9
 
-    0x20; // F Store.c
-    0x00; // 10 R0
-    0x10; // 11 0x10
+(*0xF*)    Store_c; 0x00; 0x10; // Store.c R0 16
 
-    0x11; // 12 Load.r
-    0x00; // 13 0
+(*0x12*)   Load_r; 0x00;        // Load.r R0
 
-    0x02; // 14 Dup
-    0x32; // 15 Mul
-    0x21; // 16 Store.s
-    0x01; // 17 R1
+(*0x14*)   Dup;                 // Dup
+(*0x15*)   Mul;                 // Mul
+(*0x16*)   Store_s; 0x01;       // Store.s R1
 
-    0x11; // 18 Load.r
-    0x01; // 19 R2
+(*0x18*)   Load_r; 0x01;        // Load_r R1
 
-    0x40; // F Jump
-    0x07; // 10 7
-
+(*0x1A*)   Jump; 0x07;          // Jump 7
 ]
 
 [<EntryPoint>]
